0158e370978e4a0a62523f6eebe365a38567c2bb
Luke Fisher <lukewfisher@msn.com>
2018-06-24 18:58:25 -0600

    Closes #1. Preliminary movement and jumping.

---
+++ Charger/Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        
+
+        transform.Translate(x, 0, 0);
+
+        if (Input.GetKeyDown("space"))
+        {
+             transform.Translate(Vector3.up * 130 * Time.deltaTime, Space.World);
+        }
+    }
+}
