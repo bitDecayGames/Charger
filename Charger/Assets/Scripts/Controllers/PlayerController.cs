049befe55b02d51686bc60f12a7aaafb478e041d
Luke Fisher <lukewfisher@msn.com>
2018-06-24 19:48:28 -0600

    Closes #2. Player facing is working along with a few shitty walls for him to run into.

--- Charger/Assets/Scripts/Controllers/PlayerController.cs
+++ Charger/Assets/Scripts/Controllers/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    private float jumpDelay = 2.0f;
+    private float lastJump = 0;
+    private float facing = 1;// positive in the x direction
 	// Use this for initialization
 	void Start () {
 		
@@ -12,14 +15,29 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        SpriteRenderer playerSprite = gameObject.GetComponent<SpriteRenderer>();
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
-        
+
+        if (Input.GetAxis("Horizontal") > 0 && facing < 0)
+        {
+            playerSprite.flipX = false;
+            facing = 1;
+        }
+        else if (Input.GetAxis("Horizontal") < 0 && facing > 0)
+        {            
+            playerSprite.flipX = true;
+            facing = -1;
+        }
 
         transform.Translate(x, 0, 0);
 
-        if (Input.GetKeyDown("space"))
+        if (Input.GetKeyDown("space") && lastJump <= 0)
         {
+            lastJump = jumpDelay;
              transform.Translate(Vector3.up * 130 * Time.deltaTime, Space.World);
         }
+
+        if (lastJump > 0)
+            lastJump -= Time.deltaTime;
     }
 }
