59f26a2e22d0bc3e74dbec7a70963898718ea211
Luke Fisher <lukewfisher@msn.com>
2018-07-01 21:56:18 -0600

    #5. removed all downloaded assets from the project. Added Flash game object to player that is toggleable. Need to make it only collide with enemies.

--- Charger/Assets/Scripts/Controllers/PlayerController.cs
+++ Charger/Assets/Scripts/Controllers/PlayerController.cs
@@ -4,30 +4,80 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    private float jumpDelay = 2.0f;
+    private float jumpDelay = 0.5f;
     private float lastJump = 0;
-    private float facing = 1;// positive in the x direction
+   
     private bool grounded = true;
     private float previousY = 0;// used to determine if grounded
-	// Use this for initialization
-	void Start () {
-		
-	}
+
+    private float jumpHeight = 8.0f;
+    private float jumpDistnce = 2.5f;
+
+    private float facing = 1;// positive in the x direction
+    private float previousFacing;
+    private bool flashLightOn = false;
+    // Use this for initialization
+    Vector3 m_NewForce;
+    Rigidbody2D m_Rigidbody;
+    BoxCollider2D m_BoxCollider;
+    GameObject m_flash;
+    Vector3 yAxis = new Vector3(0,1,0);
+    Vector3 playerX;
+
+
+    void Start () {
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+        m_BoxCollider = GetComponent<BoxCollider2D>();
+        m_flash = gameObject.transform.Find("Flash").gameObject;
+        m_NewForce = new Vector3(jumpDistnce*facing , jumpHeight, 0.0f);
+        previousFacing = facing;
+        playerX = new Vector3(gameObject.transform.position.x, 0, 0);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
         SpriteRenderer playerSprite = gameObject.GetComponent<SpriteRenderer>();
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        float x;
+        playerX = new Vector3(gameObject.transform.position.x, 0, 0);
+        if (Input.GetKey(KeyCode.D))
+        {
+            x = 1f * Time.deltaTime * 4.0f;
+        }else if (Input.GetKey(KeyCode.A))
+        {
+            x = 1f * Time.deltaTime * 4.0f;
+        }
+        else
+        {
+            x = 0f * Time.deltaTime * 4.0f;
+        }
+        
+        if(Input.GetMouseButtonDown(0))
+        {
+            if (flashLightOn)
+            {
+                flashLightOn = false;
+                m_flash.SetActive(flashLightOn);
+            }
+            else
+            {
+                flashLightOn = true;
+                m_flash.SetActive(flashLightOn);
+            }                
+        }
 
+        Vector3 origin = new Vector3(0.0f, 0, 0);
         if (Input.GetAxis("Horizontal") > 0 && facing < 0)
         {
-            playerSprite.flipX = false;
+            gameObject.transform.RotateAround(playerX, Vector3.up,180f);
+            previousFacing = facing;
             facing = 1;
         }
         else if (Input.GetAxis("Horizontal") < 0 && facing > 0)
-        {            
-            playerSprite.flipX = true;
+        {
+            gameObject.transform.RotateAround(playerX, Vector3.up, 180f);
+            previousFacing = facing;
             facing = -1;
         }
 
@@ -36,17 +86,24 @@ public class PlayerController : MonoBehaviour {
         if (Input.GetKeyDown("space") && lastJump <= 0 && grounded)
         {
             lastJump = jumpDelay;
-             transform.Translate(Vector3.up * 130 * Time.deltaTime, Space.World);
+            //Use Impulse as the force on GameObject
+            m_Rigidbody.AddForce(m_NewForce, ForceMode2D.Impulse);
+
         }
 
         if (lastJump > 0)
             lastJump -= Time.deltaTime;
+        
 
         if (transform.position.y != previousY)        
             grounded = false;
         else
             grounded = true;
 
+        
+
         previousY = transform.position.y;
+        m_NewForce = new Vector3(jumpDistnce * facing, jumpHeight, 0.0f);
     }
+
 }
